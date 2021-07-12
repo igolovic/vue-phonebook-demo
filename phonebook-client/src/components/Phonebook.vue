235eaf10905136b175893a749d23672395edc1a8
Ivan Golović <igolovic@gmail.com>
2021-07-13 00:51:24 +0200

    Finished implementation and added Bootstrap

--- phonebook-client/src/components/Phonebook.vue
+++ phonebook-client/src/components/Phonebook.vue
@@ -1,52 +1,53 @@
 <template>
- <div>
-   <ul class="horizontal">
-     <li>
-        <label for="pageSizes" >Page size:</label>
-        <select v-model="selectedPageSize" name="pageSizes" id="pageSizes">
-            <option id="5">5</option>
-            <option id="10">10</option>
-          </select>
-     </li>     
-     <li>
-        <button v-on:click="prevPage" :disabled="disablePrevBtn">Previous page</button> 
-        <button v-on:click="nextPage" :disabled="disableNextBtn">Next</button>
-     </li>     
-     <li>
-       <label for="filterText" >Filter:</label>
-      <input id="filterText" name="filterText" type="search" v-model="filter">
-     </li>
-      <li>
-      <button type="button" v-on:click="filter=''" >Clear</button>
-     </li>
-     <li>
-     </li>
-     <li>
-        <label for="operators" >Current operator:</label>
-        <select v-model="selectedEmployeeWithPermissions" name="operators" id="operators" class="form-control operator">
-            <option v-for="employee in employeesWithPermissions" :key="employee.Identifier" :value="employee.Identifier">{{ employee.Name }}</option>
-          </select>
-     </li>
-   </ul>
+ <b-container class="bv-example-row">
 
-<div>
-    <table id="mainTable">
-      <thead>
-        <tr>
-          <th v-for="col in columns" v-on:click="sortTable(col.property)" :key="col.property" :value="col.header">
-            {{col.header}}<div class="arrow" v-if="col.property == sortColumn" v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"></div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="employee in sortedEmployees" :key="employee.Identifier" :value="employee.Name">
-          <td v-for="col in columns" :key="col.property" :value="col.header">{{employee[col.property]}}</td>
-        </tr>        
-      </tbody>
-    </table>  
-</div>
+<b-row class="text-center" align="center">
+   <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+    <b-input-group size="sm" prepend="Number of items on page:" class="mx-1">
+      <b-form-select v-model="selectedPageSize" name="pageSizes" id="pageSizes" right text="Number of items on page">
+        <b-form-select-option value="5">5</b-form-select-option>
+        <b-form-select-option value="10">10</b-form-select-option>
+      </b-form-select>
+    </b-input-group>
+    <b-button-group size="sm" class="mx-1">
+      <b-button v-on:click="prevPage" :disabled="disablePrevBtn">Previous page</b-button>
+      <b-button v-on:click="nextPage" :disabled="disableNextBtn">Next page</b-button>
+    </b-button-group>
+    <b-input-group size="sm" prepend="Filter:" class="mx-1">
+      <b-form-input id="filterText" name="filterText" type="search" v-model="filter" value="" class="text-left"></b-form-input>
+      <b-button v-on:click="filter=''">Clear</b-button>
+    </b-input-group>
+    <b-input-group size="sm" prepend="Application operators:" class="mx-1">
+    <b-form-select v-model="selectedEmployeeWithPermissions" name="operators" id="operators" right text="Operators">
+      <b-form-select-option v-for="employee in employeesWithPermissions" :key="employee.Identifier" :value="employee.Identifier">{{employee.Name}} {{employee.LastName}}</b-form-select-option>
+    </b-form-select>
+    </b-input-group>
+   </b-button-toolbar>
+</b-row>
 
- </div>
+<b-row>
+      <table id="mainTable">
+        <thead>
+          <tr>
+            <th v-for="col in columns" v-on:click="sortTable(col.property)" :key="col.property" :value="col.header">
+              {{col.header}}<div class="arrow" v-if="col.property == sortColumn" v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"></div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="employee in sortedEmployees" :key="employee.Identifier" :value="employee.Name">
+            <td v-for="col in columns" :key="col.property" :value="col.header">{{employee[col.property]}}</td>
+          </tr>        
+        </tbody>
+      </table>  
+</b-row>
+<b-row>
+  <label v-if="hasFilteredEmployees">
+    Showing page {{currentPage}} of {{showingTo}} for total of {{filteredEmployees.length}} records
+  </label>
+</b-row>
+
+ </b-container>
 </template>
 
 <script>
@@ -82,7 +83,6 @@ export default {
     },
     prevPage:function prevPage() {
       if(this.currentPage > 1) this.currentPage--;
-      // if(this.currentPage == 1)
     }
   },
   watch:{
@@ -98,6 +98,10 @@ export default {
     },
     filter:function filter() {
       // Filtering applied - set to first page
+      this.currentPage = 1;
+    },
+    selectedPageSize:function selectedPageSize() {
+      // Page size changed - set to first page
       this.currentPage = 1;
     }
   },
@@ -136,13 +140,22 @@ export default {
         if(index >= start && index < end) return true;
       });
     },
+    hasFilteredEmployees: function (){
+      return this.filteredEmployees.length > 0;
+    },
     disablePrevBtn: function (){
-      return this.sortedEmployees.length == 0 || this.currentPage == 1;
+      return this.filteredEmployees.length == 0 || this.currentPage == 1;
     },
     disableNextBtn: function (){
-      return this.sortedEmployees.length == 0 || (this.selectedPageSize * this.currentPage) > this.filteredEmployees.length;
+      let difference = this.filteredEmployees.length - (Number(this.selectedPageSize) * this.currentPage);
+      return this.filteredEmployees.length == 0 || difference <= 0;
+    },
+    showingTo: function (){
+      let div = this.filteredEmployees.length / this.selectedPageSize;
+      let whole = Math.floor(div);
+      return whole + (((this.filteredEmployees.length % this.selectedPageSize) > 0) ? 1 : 0);
     },    
-    columns:function columns() {
+    columns:function () {
       if (this.employees.length == 0) {
         return [];
       }
